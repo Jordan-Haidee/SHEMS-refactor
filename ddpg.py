a9697e69fb18cd79533b186a0723572666228a07
Jordan <2807427568@qq.com>
2023-11-06 14:52:58 +0800

    Refactor epsilon setting policy.

--- ddpg.py
+++ ddpg.py
@@ -2,13 +2,18 @@ import random
 from collections import deque
 from datetime import datetime
 from pathlib import Path
-from typing import *
+from typing import Union
 import gymnasium as gym
 import numpy as np
 import torch
 from torch import nn, optim
 from torch.utils import tensorboard as tb
 from tqdm import tqdm
+
+"""
+TODO: 添加动作噪声Gauss/epsilon
+TODO: 评估方式
+"""
 
 
 class ReplayBuffer:
@@ -45,6 +50,30 @@ class ReplayBuffer:
 class Actor(nn.Module):
     """actor网络"""
 
+    # def __init__(self, state_dim: int, hidden_dim: list, action_bound: list):
+    #     super().__init__()
+    #     assert len(hidden_dim) == 2
+    #     self.shared_layers = []
+    #     self.shared_layers.append(nn.Linear(state_dim, hidden_dim[0]))
+    #     for idx in range(len(hidden_dim)):
+    #         self.shared_layers.append(nn.ReLU())
+    #         if idx == len(hidden_dim) - 1:
+    #             self.shared_layers.append(nn.Linear(hidden_dim[idx], len(action_bound)))
+    #         else:
+    #             self.shared_layers.append(nn.Linear(hidden_dim[idx], hidden_dim[idx + 1]))
+    #     self.shared_layers = nn.Sequential(*self.shared_layers)
+
+    #     self.tanh = nn.Tanh()
+    #     self.sigmoid = nn.Sigmoid()
+    #     # action_bound是环境可以接受的动作最大值
+    #     self.action_bound = action_bound
+
+    # def forward(self, state_tensor):
+    #     x = self.shared_layers(state_tensor)
+    #     x[0] = self.action_bound[0] * self.tanh(x[0])
+    #     x[1] = self.action_bound[1] * self.sigmoid(x[1])
+    #     return x
+
     def __init__(self, state_dim, hidden_dim, action_bound: np.ndarray):
         super().__init__()
         self.fc1 = nn.Linear(state_dim, hidden_dim)
@@ -75,6 +104,30 @@ class Actor(nn.Module):
 class Critic(nn.Module):
     """Q网络: (s,a)-->q"""
 
+    # def __init__(self, state_dim, hidden_dim, action_dim):
+    #     super().__init__()
+    #     assert len(hidden_dim) == 2
+    #     self.state_layers = nn.Sequential(
+    #         nn.Linear(state_dim, hidden_dim[0]),
+    #         nn.ReLU(),
+    #         nn.Linear(hidden_dim[0], hidden_dim[1]),
+    #         # nn.ReLU(),
+    #     )
+    #     self.action_layers = nn.Sequential(
+    #         nn.Linear(action_dim, hidden_dim[-1]),
+    #         # nn.ReLU(),
+    #     )
+    #     self.shared_layers = nn.Sequential(
+    #         nn.Linear(hidden_dim[-1], hidden_dim[-1]),
+    #         nn.ReLU(),
+    #         nn.Linear(hidden_dim[-1], 1),
+    #     )
+
+    # def forward(self, state_tensor, action_tensor):
+    #     x = self.state_layers(state_tensor)
+    #     y = self.action_layers(action_tensor)
+    #     v = self.shared_layers(x + y)
+    #     return v
     def __init__(self, state_dim, hidden_dim, action_dim):
         super().__init__()
         self.fc1 = nn.Linear(state_dim + action_dim, hidden_dim)
@@ -100,10 +153,9 @@ class DDPG:
         env: Union[gym.Env, str],
         heter: np.ndarray = np.array([0.5, 0.5, 0.5]),
         lr: float = 1e-3,
-        sigma: float = 0.10,
         tau: float = 0.005,
         gamma: float = 0.98,
-        hidden_dim: int = 400,
+        hidden_dim: list = [400, 400],
         buffer_capicity: int = 10000,
         buffer_init_ratio: float = 0.30,
         batch_size: int = 64,
@@ -111,12 +163,8 @@ class DDPG:
         **kwargs,
     ):
         self.init_params = self.get_hyperparams(locals())
-        if isinstance(env, str):
-            self.env = gym.make(env, heter=heter)
-            self.env_name = env
-        else:
-            self.env = env
-            self.env_name = self.env.spec.id
+        self.env = gym.make(env, heter=heter)
+        self.env_name = env
 
         state_dim = self.env.observation_space.shape[0]
         action_dim = self.env.action_space.shape[0]
@@ -129,18 +177,16 @@ class DDPG:
         self.critic_target.load_state_dict(self.critic.state_dict())
         self.actor_optimizer = optim.Adam(self.actor.parameters(), lr / 10)
         self.critic_optimizer = optim.Adam(self.critic.parameters(), lr)
-        self.sigma = sigma
         self.tau = tau
         self.gamma = gamma
         self.device = device
         self.replay_buffer = ReplayBuffer(buffer_capicity)
         self.buffer_init_ratio = buffer_init_ratio
         self.batch_size = batch_size
-
         # 训练时使用
-        self.save_dir = (
-            Path("result") / f"DDPG-{self.env_name.replace('envs:','')}-{datetime.now().strftime(r'%Y-%m-%d-%H-%M-%S')}"
-        )
+        self.save_dir = Path(
+            "result"
+        ) / f"DDPG-{self.env_name.replace('envs:','')}-{datetime.now().strftime(r'%Y-%m-%d-%H-%M-%S')}"
         self.episode = 0
         self.episode_reward = 0
         self.episode_len = 0
@@ -154,13 +200,18 @@ class DDPG:
         return {k: v for k, v in dic.items() if k not in ["self", "env", "kwargs"]}
 
     @torch.no_grad()
-    def get_action(self, s):
+    def get_action(self, s: Union[np.array, torch.Tensor], eps: float = 0.0):
         """在训练时得到含噪声的连续动作"""
         if isinstance(s, np.ndarray) is True:
             s = torch.from_numpy(s).float().to(self.device)
-        a = self.actor(s) + torch.normal(0.0, self.sigma, a.shape)
-        a = a.cpu().numpy()
+        if np.random.uniform() < eps:
+            a = self.env.action_space.sample()
+        else:
+            s_ = self.env.unwrapped.normalize_state(s)
+            a = self.actor(s_)
+            a = a.cpu().numpy()
         s = s.cpu().numpy()
+        # ---------------------------------------------------------------------------
         p_ess, p_hvac = a
         p_solar, p_load, ess_level, temp_outdoor, temp_indoor, price, _ = s
         if p_ess >= 0:
@@ -195,10 +246,15 @@ class DDPG:
         s, _ = self.env.reset()
         with tqdm(range(int(num))) as bar:
             while self.replay_buffer.size < num:
-                a = self.env.action_space.sample()
+                a = self.get_action(s, 1.0)
                 ns, r, t1, t2, _ = self.env.step(a)
-                # self.replay_buffer.push(s, a, r, ns, t1)
-                self.replay_buffer.push(self.env.unwrapped.normalize_state(s), a, r, ns, t1)
+                self.replay_buffer.push(
+                    self.env.unwrapped.normalize_state(s),
+                    a,
+                    r,
+                    self.env.unwrapped.normalize_state(ns),
+                    t1,
+                )
                 s = ns if not t2 else self.env.reset()[0]
                 bar.update()
 
@@ -208,6 +264,13 @@ class DDPG:
         for net, net_ in net_groups:
             for p, p_ in zip(net.parameters(), net_.parameters()):
                 p_.data.copy_(p.data * self.tau + p_.data * (1 - self.tau))
+
+    @property
+    def epsilon(self):
+        """得到递减的epsilon"""
+        prog = self.global_step / self.total_time_steps
+        assert 0 <= prog <= 1.0
+        return max(np.exp(-4 * prog), 0.10)
 
     def train_one_batch(self):
         # 从buffer中取出一批数据
@@ -231,6 +294,36 @@ class DDPG:
         self.soft_sync_target()
         return actor_loss.detach(), critic_loss.detach()
 
+    def train(self, total_time_steps: int, evaluation_interval: int):
+        # 训练前准备
+        self.total_time_steps = total_time_steps
+        self.save_dir.mkdir(parents=True)
+        self.logger = tb.SummaryWriter(self.save_dir / "train")
+        self.evaluation_interval = evaluation_interval
+        self.collect_exp_before_train()
+        self.bar = tqdm(range(total_time_steps), ncols=80)
+        # 开始训练
+        s, _ = self.env.reset()
+        while self.global_step < self.total_time_steps:
+            a = self.get_action(s, self.epsilon)
+            ns, r, t1, t2, _ = self.env.step(a)
+            self.episode_reward += r
+            self.replay_buffer.push(
+                self.env.unwrapped.normalize_state(s),
+                a,
+                r,
+                self.env.unwrapped.normalize_state(ns),
+                t1,
+            )
+            if t1 or t2:
+                self.log_info_per_episode()
+                s, _ = self.env.reset()
+            else:
+                s = ns
+            actor_loss, critic_loss = self.train_one_batch()
+            self.log_info_per_batch(actor_loss, critic_loss)
+        self.save(self.save_dir / "latest.pt")
+
     def log_info_per_episode(self):
         self.logger.add_scalar("Train/episode_reward", self.episode_reward, self.episode)
         self.logger.add_scalar("Train/buffer_size", self.replay_buffer.size, self.episode)
@@ -238,7 +331,7 @@ class DDPG:
         self.bar.set_description_str(f"episode->{self.episode+1:0>5d}|")
         self.bar.set_postfix_str(f"reward->{self.episode_reward:8.1f}")
         if self.episode == 0:
-            self.max_episode_reward = self.episode_reward
+            self.max_episode_reward = -(10**10)
         if self.episode_reward >= self.max_episode_reward:
             self.max_episode_reward = self.episode_reward
             self.save(self.save_dir / "best.pt")
@@ -249,37 +342,15 @@ class DDPG:
     def log_info_per_batch(self, actor_loss, critic_loss):
         self.logger.add_scalar("Loss/actor_loss", actor_loss, self.global_step)
         self.logger.add_scalar("Loss/critic_loss", critic_loss, self.global_step)
+        self.logger.add_scalar("Loss/critic_loss", critic_loss, self.global_step)
+        self.logger.add_scalar("Train/epsilon", self.epsilon, self.global_step)
         if self.global_step % self.evaluation_interval == 0:
             evaluation_reward = self.test(is_render=False, print_log=False)
             self.logger.add_scalar("Evaluation/episode_reward", evaluation_reward, self.global_step)
-            self.save(self.save_dir / f"episode-{self.episode}-reward-{self.episode_reward:.2f}.pt")
+            # self.save(self.save_dir / f"episode-{self.episode}-reward-{self.episode_reward:.2f}.pt")
         self.global_step += 1
         self.episode_len += 1
         self.bar.update()
-
-    def train(self, total_time_steps: int, evaluation_interval: int):
-        # 训练前准备
-        self.save_dir.mkdir(parents=True)
-        self.logger = tb.SummaryWriter(self.save_dir / "train")
-        self.evaluation_interval = evaluation_interval
-        self.collect_exp_before_train()
-        self.bar = tqdm(range(total_time_steps), ncols=80)
-        # 开始训练
-        s, _ = self.env.reset()
-        while self.global_step < total_time_steps:
-            # a = self.get_action(s)
-            a = self.get_action(self.env.unwrapped.normalize_state(s))
-            ns, r, t1, t2, _ = self.env.step(a)
-            self.episode_reward += r
-            # self.replay_buffer.push(s, a, r, ns, t1)
-            self.replay_buffer.push(self.env.unwrapped.normalize_state(s), a, r, ns, t1)
-            if t1 or t2:
-                self.log_info_per_episode()
-                s, _ = self.env.reset()
-            else:
-                s = ns
-            actor_loss, critic_loss = self.train_one_batch()
-            self.log_info_per_batch(actor_loss.detach(), critic_loss.detach())
 
     def save(self, save_path: str, save_hyper: bool = True):
         """保存模型权重/超参数"""
@@ -305,13 +376,6 @@ class DDPG:
         model.critic.load_state_dict(weights.get("critic"))
         return model
 
-    @torch.no_grad()
-    def predict(self, state_tensor):
-        """输入状态输出动作"""
-        state_tensor = state_tensor.to(self.device)
-        action = self.actor(state_tensor).cpu().numpy()
-        return action
-
     def test(
         self,
         device: str = None,
@@ -332,14 +396,14 @@ class DDPG:
         s, _ = env.reset()
         iter_num = 0
         while True:
-            a = self.predict(torch.from_numpy(s).float().to(self.device))
+            a = self.get_action(s, eps=0.0)
             s, r, t1, t2, _ = env.step(a)
             total_reward += r
             iter_num += 1
             if is_render:
                 env.render()
             if print_log:
-                print(iter_num, r, s)
+                print(iter_num, a.round(4), s.round(4), round(r, 4))
             if (truncated_exit and t2) or t1:
                 break
         return total_reward
