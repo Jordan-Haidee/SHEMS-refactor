fb6ad19b49d62933e21db0013c8e680069be82cb
Jordan <2807427568@qq.com>
2023-11-10 13:47:37 +0800

    Initially add a test script.

--- test.py
+++ test.py
@@ -1,13 +1,45 @@
-from matplotlib import pyplot as plt
-import numpy as np
+from argparse import ArgumentParser
 from pathlib import Path
 
-save_dir = Path("result/backup/baselines/baseline-0")
-env_num = 10
+import gymnasium as gym
+import numpy as np
+import pytoml
+import torch
+from addict import Dict
 
-data = np.vstack([np.load(save_dir / f"point-{i}" / "episode_reward_list.npy") for i in range(env_num)])
-label = [i for i in range(env_num)]
-plt.plot(data.mean(axis=0), label="Avg", marker="^")
-# plt.plot(data.transpose(), label=label)
-plt.legend(), plt.grid()
-plt.show()
+from model import Actor
+
+parser = ArgumentParser(description="Test FedDDPG Algo & Baseline")
+parser.add_argument("save_dir", type=str, help="Exp result save directory")
+args = parser.parse_args()
+
+save_dir = Path(args.save_dir)
+with open(save_dir / "config_backup.toml") as f:
+    config = Dict(pytoml.load(f))
+env = gym.make(config.env)
+state_dim = env.observation_space.shape[0]
+action_dim = env.action_space.shape[0]
+action_bound = env.action_space.high
+total_r = []
+for i in range(config.env_num):
+    env = gym.make(config.env, heter=config.heter_set[i], is_test=True)
+    actor = Actor(state_dim, 400, action_bound)
+    try:
+        actor.load_state_dict(torch.load(save_dir / f"point-{i}" / "latest.pt").get("actor"))
+    except TypeError:
+        actor.load_state_dict(torch.load(save_dir / f"point-{i}" / "latest.pt").get("weights").get("actor"))
+    s, _ = env.reset()
+    point_r = 0
+    with torch.no_grad():
+        while True:
+            a = actor(torch.from_numpy(env.unwrapped.normalize_state(s))).numpy()
+            ns, r, t1, t2, info = env.step(a)
+            print(a.round(4), ns.round(4), round(info["c1"], 4), round(info["c2"], 4), round(info["c3"], 4))
+            s = ns
+            point_r += r
+            if t1 or t2:
+                total_r.append(point_r)
+                break
+total_r = np.array(total_r)
+print(total_r.round(4))
+print(total_r.mean())
