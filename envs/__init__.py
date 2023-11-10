be813d80e1915024a21ec5ec37389dcd0f0b890c
Jordan <2807427568@qq.com>
2023-11-10 13:49:40 +0800

    Make action-restrct wrapper actually available.

--- envs/__init__.py
+++ envs/__init__.py
@@ -10,5 +10,10 @@ register(
             entry_point="envs.wrappers:MaxEpisodeStepsLimit",
             kwargs={"max_episode_steps": None},
         ),
+        WrapperSpec(
+            name="ActionRestrict",
+            entry_point="envs.wrappers:ActionRestrict",
+            kwargs={"args": None},
+        ),
     ),
 )
--- envs/wrappers/action_restrict.py
+++ envs/wrappers/action_restrict.py
@@ -1,20 +1,23 @@
 import numpy as np
+
 import gymnasium as gym
 
-class ActionRestrict(gym.ActionWrapper):
-    def __init__(self, env):
-        super().__init__(env)
+
+class ActionRestrict(gym.ActionWrapper, gym.utils.RecordConstructorArgs):
+    def __init__(self, env: gym.Env, args=None):
+        gym.utils.RecordConstructorArgs.__init__(self, args=args)
+        gym.ActionWrapper.__init__(self, env)
 
     def action(self, a):
         p_ess, p_hvac = a
-        p_solar, p_load, ess_level, temp_outdoor, temp_indoor, price, _ = self.unwrapped.state
+        p_solar, p_load, ess_level, temp_outdoor, temp_indoor, price, _ = self.env.unwrapped.state
         if p_ess >= 0:
             p_ess = np.clip(
                 p_ess,
                 0,
                 min(
-                    (self.unwrapped.ess_level_max - ess_level) / self.unwrapped.eta_ess,
-                    self.unwrapped.p_ess_max,
+                    (self.env.unwrapped.ess_level_max - ess_level) / self.env.unwrapped.eta_ess,
+                    self.env.unwrapped.p_ess_max,
                 ),
             )
         else:
@@ -22,12 +25,12 @@ class ActionRestrict(gym.ActionWrapper):
                 -p_ess,
                 0,
                 min(
-                    (ess_level - self.unwrapped.ess_level_min) * self.unwrapped.eta_ess,
-                    self.unwrapped.p_ess_max,
+                    (ess_level - self.env.unwrapped.ess_level_min) * self.env.unwrapped.eta_ess,
+                    self.env.unwrapped.p_ess_max,
                 ),
             )
-        if temp_indoor <= self.unwrapped.T_min:
+        if temp_indoor <= self.env.unwrapped.T_min:
             p_hvac = 0
-        if temp_indoor > self.unwrapped.T_max:
-            p_hvac = np.clip(p_hvac, 0.1, self.unwrapped.p_hvac_max)
+        if temp_indoor > self.env.unwrapped.T_max:
+            p_hvac = np.clip(p_hvac, 0.1, self.env.unwrapped.p_hvac_max)
         return np.array([p_ess, p_hvac])
